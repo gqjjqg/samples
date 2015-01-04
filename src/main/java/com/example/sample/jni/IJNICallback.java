85753cbdf3c7dc661372c20538bf5e4fb4f9b50e
gqjjqg <qijiang.guo@gmail.com>
2015-01-04 14:32:50 +0800

    update Signed-off-by: gqjjqg <qijiang.guo@gmail.com>

--- src/main/java/com/example/sample/jni/IJNICallback.java
+++
@@ -1,5 +0,0 @@
-package com.example.sample.jni;
-
-public interface IJNICallback {
-	public abstract int callTest(int val, int val2);
-}
--- src/main/jni/Android.mk
+++
@@ -1,8 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE    := sample
-LOCAL_SRC_FILES := sample.cpp
-
-include $(BUILD_SHARED_LIBRARY)
